ef1e330720b779c4f77b075309b025147ad3cd4c
Jfoy <jamesfoy17@gmail.com>
2018-10-10 12:01:59 +0100

    TernaryOperator
    Finished

---
+++ Assets/Scripts/TernaryOperator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TernaryOperator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        int health = 10;
+        string message;
+
+        message = health > 0 ? "Player is Alive" : health == 0 ? "Player is Barely Alive" : "Player is Dead";
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
